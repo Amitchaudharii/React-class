9a7f4dd98415357ab702595dc34c7dc9cf0d592b
Amit <chaudhary.amit2058@gmail.com>
2022-04-07 00:16:23 +0545

    Feat: React select

--- src/BillingList.js
+++ src/BillingList.js
@@ -1,5 +1,8 @@
 import React, { useEffect, useState } from "react";
 import "./BillingList.css";
+import moment from "moment";
+import Select from 'react-select'
+import ReactSelect from "react-select";
 
 const BillingList = () => {
   const [entries, setEntries] = useState([]);
@@ -37,17 +40,9 @@ const BillingList = () => {
       <div className="main-container">
         <div className="box">
           <h1>Billing List</h1>
-          {entries.map((en) => (
-            <div>
-              <span>{en.productName}</span>
-              <span>{+en.quantity}</span>
-              <span>{+en.price}</span>
-              <span>{+en.price * +en.quantity}</span>
-            </div>
-          ))}
 
           <div className="billinglist-form">
-            <select
+            {/* <select
               value={product}
               onChange={(e) => setProduct(e.target.value)}
             >
@@ -56,13 +51,25 @@ const BillingList = () => {
                   {p.name}
                 </option>
               ))}
-            </select>
+            </select> */}
+            <ReactSelect options={products.map(p => ({
+              ...p,
+              value: p.id,
+              label: p.name,
+            }))}
+            onChange={a => setProduct(a.id)}
+            placeholder={"Select product"}
+            />
             <input
               value={quantity}
               type="number"
               onChange={(e) => setQuantity(e.target.value)}
             />
-
+            <div className="add-btn"actSelect>
+              <button className="add-btn01" onClick={handleAddEntry}>
+                Add Entry
+              </button>
+            </div>
             <input
               value={discountRate}
               type="number"
@@ -73,13 +80,26 @@ const BillingList = () => {
               type="number"
               onChange={(e) => setVatRate(e.target.value)}
             />
-            <div className="add-btn">
-              <button className="add-btn01" onClick={handleAddEntry}>
-                Add Entry
-              </button>
-            </div>
           </div>
           <div className="billinglist-details">
+            <div className="list-details">
+              <ul>
+                <li>Date</li>
+                <li>Product</li>
+                <li>Quantity</li>
+                <li>Price</li>
+                <li>Total</li>
+              </ul>
+              {entries.map((en) => (
+                <div key={en.id} className="car-details">
+                  <span>{moment().format("MM DD, YYYY")}</span>
+                  <span>{en.productName}</span>
+                  <span>{+en.quantity}</span>
+                  <span>{+en.price}</span>
+                  <span>{+en.price * +en.quantity}</span>
+                </div>
+              ))}
+            </div>
             <div className="car-details">
               <span>subTotal</span>
               <span>
